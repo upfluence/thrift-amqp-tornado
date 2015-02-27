9aa519912efb6af67eebe965c17eb321ce855a69
Alexis Montagne <alexis.montagne@gmail.com>
2015-02-27 15:28:32 +0100

    Wellform the package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='thrift_amqp_tornado',
+      version='0.0.1',
+      description='Thirft transport implementation over the AMQP protocol',
+      author='Alexis Montagne',
+      author_email='alexis.montagne@upfluence.co',
+      url='https://github.com/upfluence/thrift-amqp-tornado',
+      packages=['thrift_amqp_tornado'])
