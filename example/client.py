f70590e19052d32ff1309f018d00ac70b992539c
Alexis Montagne <alexis.montagne@gmail.com>
2015-04-10 15:49:39 +0200

    Update the example

--- example/client.py
+++ example/client.py
@@ -7,40 +7,20 @@ from thrift.protocol import TJSONProtocol
 from thrift_amqp_tornado import TAMQPTornadoTransport
 
 
-def this_joint():
-    communicate(callback=io_loop.stop)
-
-
-@gen.engine
-def resolve(i, f, callback):
-    yield f
-    print i
-    callback()
-
-
-@gen.engine
-def communicate(callback):
+@gen.coroutine
+def communicate():
     transport = TAMQPTornadoTransport()
     pfactory = TJSONProtocol.TJSONProtocolFactory()
     client = Scrapper.Client(transport, pfactory)
 
     yield gen.Task(transport.open)
 
-    futures = []
+    futures = [client.scrape('http://google.com/') for i in xrange(100)]
 
-    for i in xrange(100):
-        f = client.scrape('http://google.com/')
-        futures.append(f)
-
-    i = 0
-
-    for f in futures:
-        i += 1
-        yield gen.Task(resolve, i, f)
+    yield futures
 
     client._transport.close()
 
-    callback()
-
-io_loop.add_callback(this_joint)
+    io_loop.stop()
+io_loop.add_callback(communicate)
 io_loop.start()
