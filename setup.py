af040f41ea5f00ffd4753f762c8bb9dcc8b88d0c
Alexis Montagne <alexis.montagne@gmail.com>
2016-04-22 17:12:01 -0400

    Use pika-upfluence over pika

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='thrift_amqp_tornado',
-      version='0.1.7',
+      version='0.2.0',
       description='Thirft transport implementation over the AMQP protocol',
       author='Alexis Montagne',
       author_email='alexis.montagne@upfluence.co',
       url='https://github.com/upfluence/thrift-amqp-tornado',
       packages=['thrift_amqp_tornado'],
-      install_requires=['thrift', 'pika', 'toro', 'futures'])
+      install_requires=['thrift', 'pika-upfluence', 'toro', 'futures'])
