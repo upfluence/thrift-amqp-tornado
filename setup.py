371d4eec3729b387e7b966915849b8137185f7b8
Alexis Montagne <alexis.montagne@gmail.com>
2016-04-25 18:33:57 -0700

    Bump version to 0.2.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='thrift_amqp_tornado',
-      version='0.2.3',
+      version='0.2.4',
       description='Thirft transport implementation over the AMQP protocol',
       author='Alexis Montagne',
       author_email='alexis.montagne@upfluence.co',
